e4b32b2a77449b5a09151184b3b7878a3ee9ba8d
zhuhongxu <zhuhx@degnity.com>
2019-01-18 14:14:24 +0800

    map和flatMap的区别

--- src/main/java/com/zhx/chapter11/MapDemo2.scala
+++ src/main/java/com/zhx/chapter11/MapDemo2.scala
@@ -9,7 +9,8 @@ object MapDemo2 {
   def main(args: Array[String]): Unit = {
 //    test1
 //    test2
-    test3
+//    test3
+    test4
   }
 
   /**
@@ -58,6 +59,28 @@ object MapDemo2 {
 
   def startWithA(param : String): Boolean={
     param.startsWith("a")
+  }
+
+  /**
+    * 测试map和flatMap的区别
+    */
+  def test4: Unit ={
+    //reference: https://blog.csdn.net/bitcarmanlee/article/details/52983607
+    val li = List(1,2,3)
+
+    //flatMap中接受的函数的返回值只能是List，否则不能通过编译，并且flatMap会把返回的List平铺开，但是map不会，返回是什么样就是什么样
+    val flatMapRes = li.flatMap(x => x match {
+      case 3 => List('a','b')
+      case _ => List(x*2)
+    })
+    println("flatMap : " + flatMapRes)
+
+
+    val mapRes = li.map(x => x match {
+      case 3 => List('a','b')
+      case _ => x*2
+    })
+    println("map : " + mapRes)
   }
 
 }
